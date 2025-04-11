67979232d1d6fbafd8750168148eaf3fa28301c5
Sebastien Houssein <housseinseb@icloud.com>
2025-04-11 10:23:57 +0200

    Adding methods to Database

--- Database.php
+++ Database.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class Database extends PDO
 {
     private string $host;
--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return[
-    'host' => '127.0.0.1',
-    'port' => 8889,
-    'user' => 'root',
-    'password' => 'root',
-    'database' => 'the_artbox_db',
-];
